10757feb85650fd0a3da47e7142d9c84b1ab8d30
Mathias Mutua <triplem706@gmail.com>
2026-04-27 10:35:38 +0300

    Simple addition servlet and jsp

---
+++ Servlets & JSPs/SimpleAdditionWebApp/src/java/com/addnums/AddServlet.java
@@ -0,0 +1,27 @@
+package com.addnums;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author mathias
+ */
+@WebServlet(name = "AddServlet", urlPatterns = {"/add"})
+public class AddServlet extends HttpServlet {
+    public void service(HttpServletRequest req, HttpServletResponse res ) throws IOException
+    {
+        int i = Integer.parseInt(req.getParameter("num1"));
+        int j = Integer.parseInt(req.getParameter("num2"));
+    
+        int k = i+j;
+        
+        PrintWriter print = res.getWriter();
+        print.println("The result is " +k);
+    }
+}
